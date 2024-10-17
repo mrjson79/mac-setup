f60fa545d025d11a63e2932ac5b82bf9d36e74f5
Anders Johansson <anders.johansson@etraveligroup.com>
2024-10-17 12:26:42 +0200

    added home.nix file

---
+++ home.nix
@@ -0,0 +1,29 @@
+# 
+ {config, pkgs, ... }:
+
+{
+  # TODO please change the username & home directory to your own
+  home.username = "andersjohansson";
+  home.homeDirectory = "/Users/andersjohansson";
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    # here is some command line tools I use frequently
+    # feel free to add your own or remove some of them
+
+    # neofetch
+  ];
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.05";
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
