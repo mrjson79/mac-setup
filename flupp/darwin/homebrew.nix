40f13295e6008ef8cd41e57c9c065f372c4ebd95
Anders Johansson <anders.johansson@etraveligroup.com>
2024-10-21 11:10:52 +0200

    target config

---
+++ flupp/darwin/homebrew.nix
@@ -0,0 +1,102 @@
+{ ... }: {
+  homebrew = {
+    enable = true;
+    global = { autoUpdate = false; };
+    # will not be uninstalled when removed
+    masApps = {
+      Xcode = 497799835;
+      Transporter = 1450874784;
+      VN = 1494451650;
+    };
+    onActivation = {
+      # "zap" removes manually installed brews and casks
+      cleanup = "zap";
+      autoUpdate = false;
+      upgrade = false;
+    };
+    brews = [
+      "sniffnet" # monitor network traffic
+      "aws-iam-authenticator" # eks login
+      "bitwarden-cli" # raycast extension does not like nix version
+      "borders" # borders for windows
+      "openai-whisper" # transcode audio to text
+      "databricks" # databricks cli
+      "pkgxdev/made/pkgx" # run anything
+
+      # ios development
+      "cocoapods"
+      "ios-deploy"
+
+      # work
+      "libmagic"
+      "ruff" # python linter
+    ];
+    casks = [
+      # utilities
+      "aldente" # battery management
+      "macfuse" # file system utilities
+      "hiddenbar" # hides menu bar icons
+      "meetingbar" # shows upcoming meetings
+      "karabiner-elements" # keyboard remap
+      "eurkey" # keyboard layout
+      "nikitabobko/tap/aerospace" # tiling window manager
+      "displaylink" # connect to external dell displays
+
+      # coding
+      "intellij-idea"
+      "postman"
+
+      # virtualization
+      "utm" # virtual machines
+      "docker" # docker desktop
+
+      # communication
+      "microsoft-teams"
+      "microsoft-auto-update"
+      "microsoft-outlook"
+      "zoom"
+      "slack"
+      "signal"
+      "discord"
+
+      "bitwarden" # password manager
+      "spotify" # music
+      "android-studio" # needed for react-native
+      "rekordbox" # dj software
+      "obs" # stream / recoding software
+      "protonmail-bridge"
+      "shottr" # screenshot tool
+      "the-unarchiver"
+      "tunnelblick" # vpn client
+      "eul" # mac monitoring
+      "wireshark" # network sniffer
+      "sf-symbols" # patched font for sketchybar
+      "prusaslicer" # slicer for my printer
+      "autodesk-fusion" # CAD tool
+      "time-out" # blurs screen every x mins
+      "raycast" # launcher on steroids
+      "keycastr" # show keystrokes on screen
+      "obsidian" # zettelkasten
+      "arc" # mac browser
+      "visual-studio-code" # code editor
+      "zed" # vim like editor
+      "vlc" # media player
+      "linear-linear" # task management
+      "balenaetcher" # usb flashing
+      "spacedrive" # file explorer
+      "steam" # gaming
+      "chatgpt" # open ai desktop client
+    ];
+    taps = [
+      # default
+      "homebrew/bundle"
+      "homebrew/cask-fonts"
+      "homebrew/services"
+      # custom
+      "FelixKratz/formulae" # borders
+      "databricks/tap" # databricks
+      "pkgxdev/made" # pkgx
+      "nikitabobko/tap" # aerospace
+    ];
+  };
+}
---
+++ flupp/shell/git.nix
@@ -0,0 +1,82 @@
+{ ... }: {
+  programs.git = {
+    enable = true;
+    delta.enable = true;
+    userName = "Felix Breuer";
+    userEmail = "fbreuer@pm.me";
+    signing = {
+      key = null; # gnupg decides by mail
+      signByDefault = true;
+    };
+    includes = [
+      {
+        condition = "gitdir:~/code/rtl/";
+        contents.user.email = "felix.breuer@rtl-extern.de";
+      }
+      {
+        condition = "gitdir:~/code/inovex/";
+        contents.user.email = "felix.breuer@inovex.de";
+      }
+      {
+        condition = "gitdir:~/code/controlf/";
+        contents = {
+          commit.gpgSign = false;
+          user = {
+            email = "felix@controlf.io";
+            name = "Felix Breuer";
+          };
+        };
+      }
+    ];
+    hooks = { prepare-commit-msg = ./rtl-hook.sh; };
+    aliases = {
+      cm = "commit";
+      ca = "commit --amend --no-edit";
+      co = "checkout";
+      cp = "cherry-pick";
+
+      di = "diff";
+      dh = "diff HEAD";
+
+      pu = "pull";
+      ps = "push";
+      pf = "push --force-with-lease";
+
+      st = "status -sb";
+      fe = "fetch";
+      gr = "grep -in";
+
+      ri = "rebase -i";
+      rc = "rebase --continue";
+    };
+    ignores = [
+      # ide
+      ".idea"
+      ".vs"
+      ".vsc"
+      ".vscode"
+      # npm
+      "node_modules"
+      "npm-debug.log"
+      # python
+      "__pycache__"
+      "*.pyc"
+
+      ".ipynb_checkpoints" # jupyter
+      "__sapper__" # svelte
+      ".DS_Store" # mac
+      "kls_database.db" # kotlin lsp
+    ];
+    extraConfig = {
+      init.defaultBranch = "main";
+      pull = {
+        ff = false;
+        commit = false;
+        rebase = true;
+      };
+      fetch = { prune = true; };
+      push.autoSetupRemote = true;
+      delta = { line-numbers = true; };
+    };
+  };
+}
